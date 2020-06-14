cfb7d56f50633d007dfe37df2839774d3524cdca
rene.ruprecht <rene.ruprecht@stud.th-luebeck.de>
2020-06-14 15:44:46 +0200

    Progressbar hinzugefügt.
    Damit der Status visualisiert werden kann, muss für das Zielverzeichnis1 wie auch für das Zielverzeichnis2 eine Progressbar angezeigt werden

--- MediaBackup/view.py
+++ MediaBackup/view.py
@@ -1,12 +1,16 @@
 from tkinter import *
+from tkinter import ttk
 
 from MediaBackup.constants import Colors
-from MediaBackup.controller import Controller
 
 
 class View:
     def __init__(self, master):
-        controller = Controller()
+        s = ttk.Style()
+        s.theme_use('clam')
+        s.configure("yellow.Horizontal.TProgressbar", foreground='yellow', background='yellow')
+        s.configure("blue.Horizontal.TProgressbar", foreground='blue', background='blue')
+
         self.frame = Frame(master)
         self.frame.grid_rowconfigure(0, weight=1, minsize=100)
         self.frame.grid_columnconfigure(0, weight=1, minsize=100)
@@ -17,17 +21,52 @@ class View:
         self.frame.grid_rowconfigure(1, weight=1, minsize=100)
         self.frame.grid(row=1, columnspan=5, sticky='nesw')
 
-        self.btn_load = Button(self.frame, text="load", bg=Colors.BUTTON_COLOR, command=lambda: controller.print())
+        # Button Quelle
+        self.btn_load = Button(self.frame, text="load", bg=Colors.BUTTON_COLOR, command=lambda: "")
         self.btn_load.grid(row=0, column=0, columnspan=1, sticky='nesw')
 
+        # Button Zielverzeichnis 1
         self.btn_dest1 = Button(self.frame, text="dest1",
-                                command=lambda: self.btn_load.grid_forget())
+                                command=lambda: "")
         self.btn_dest1.grid(row=0, column=1, columnspan=2, sticky='nesw')
 
+        self.progressbar_dest1 = ttk.Progressbar(self.frame, style="blue.Horizontal.TProgressbar",
+                                                 orient="horizontal",
+                                                 mode="determinate", maximum=100, value=20)
+
+        # Button Zielverzeichnis 2
         self.btn_dest2 = Button(self.frame, text="dest2",
-                                command=lambda: self.btn_load.grid(row=0, column=0, sticky='nesw'))
+                                command=lambda: self.show_btn_dest1())
         self.btn_dest2.grid(row=0, column=3, columnspan=2, sticky='nesw')
 
+        # Progressbar2
+        self.progressbar_dest2 = ttk.Progressbar(self.frame, style="blue.Horizontal.TProgressbar",
+                                                 orient="horizontal",
+                                                 mode="determinate", maximum=100, value=20)
+
+        # Listbox für die geladenen Dateien
         listbox = Listbox(self.frame)
         listbox.grid(row=1, columnspan=5, sticky='nesw')
+
         self.frame.pack(expand=True, fill="both")
+
+    # btn_dest1
+    def show_btn_dest1(self):
+        self.btn_dest1.grid(row=0, column=1, columnspan=2, sticky='nesw')
+
+    def hide_btn_dest1(self):
+        self.btn_dest1.grid_forget()
+
+    # progressbar_dest1
+    def show_progressbar_dest1(self):
+        self.progressbar_dest1.grid(row=0, column=3, columnspan=2, sticky='nesw')
+
+    def hide_progressbar_dest1(self):
+        self.progressbar_dest1.grid_forget()
+
+    # btn_dest2
+    def show_btn_dest2(self):
+        self.btn_dest2.grid(row=0, column=1, columnspan=2, sticky='nesw')
+
+    def hide_btn_dest2(self):
+        self.btn_dest2.grid_forget()
